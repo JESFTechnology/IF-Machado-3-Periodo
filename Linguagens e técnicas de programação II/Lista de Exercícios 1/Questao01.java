4322b5b841cc0f1323d9246ca84370bee240d62b
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2025-04-07 07:27:24 -0300

    Create Questao01.java

---
+++ Linguagens e técnicas de programação II/Lista de Exercícios 1/Questao01.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exerciciosjava;
+
+/**
+ *
+ * @author johan
+ */
+public class ExerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
