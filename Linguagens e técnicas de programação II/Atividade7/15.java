66177a498bfa097b87cc39e6a47bb6a93b7d38ab
JESF Technology <116103419+JESFTechnology@users.noreply.github.com>
2025-05-29 07:22:21 -0300

    Create 15.java
    Retorna a posição do elemento na fila dinâmica:

---
+++ Linguagens e técnicas de programação II/Atividade7/15.java
@@ -0,0 +1,10 @@
+public int posicaoElemento(T valor) {
+    Node<T> atual = inicio;
+    int pos = 0;
+    while(atual != null) {
+        if(atual.dado.equals(valor)) return pos;
+        atual = atual.prox;
+        pos++;
+    }
+    return -1; // não encontrado
+}
